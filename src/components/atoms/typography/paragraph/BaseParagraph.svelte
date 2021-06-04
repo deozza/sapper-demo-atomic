e8c27b396e417800f784451afb6655eb666f01ef
Edenn Touitou <deozza@gmail.com>
2021-06-04 14:23:34 +0200

    added paragraph atom component

---
+++ src/components/atoms/typography/paragraph/BaseParagraph.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import BaseParagraphModel from "./BaseParagraphModel";
+
+    export let baseParagraphModel: BaseParagraphModel;
+</script>
+
+<p class="{baseParagraphModel.style}">{baseParagraphModel.content}</p>
+
+<style>
+    .light {
+        color: black;
+    }
+    .dark {
+        color: white;
+    }
+    .primary {
+        color: var(--primary_text);
+    }
+
+    .secondary {
+        color: var(--secondary_text);
+    }
+
+    .success {
+        color: var(--success_text);
+    }
+
+    .danger {
+        color: var(--danger_text);
+    }
+
+    .warning {
+        color: var(--warning_text);
+    }
+
+    .info {
+        color: var(--info_text);
+    }
+</style>
