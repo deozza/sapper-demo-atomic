1ef4b879778825f66e9c93dcf667ac45aecfef1b
Edenn Touitou <deozza@gmail.com>
2021-06-25 01:26:56 +0200

    implemented firebase and hasura features

--- src/api/services/User.ts
+++ src/api/services/User.ts
@@ -1,3 +1,36 @@
+import {User as UserEntity} from "../entities/User";
+import {Error} from "../utils/useCaseResult/types/Error";
+import {INSERT_USERS} from "../graphqlQueries/User";
+
+import {auth} from "../../firebase";
+import firebase from "firebase";
+import UserCredential = firebase.auth.UserCredential;
+import {mutation} from "svelte-apollo"
+
 export class User {
-    
+    public async addUserToFirebase(user: UserEntity) {
+        await auth().createUserWithEmailAndPassword(user.email, user.password)
+            .then((userCredential: UserCredential) => {
+                user.uid = userCredential.user.uid
+
+                return [user, null]
+            }).catch((errorFromFirebase) => {
+                const error: Error = new Error(errorFromFirebase.code, errorFromFirebase.message)
+                return [user, error]
+            })
+    }
+
+    public async addUserToHasura(user: UserEntity){
+        const addUserToHasura = mutation(INSERT_USERS)
+        console.log('coucou')
+        try{
+            await addUserToHasura({variables: {$uuid: user.uid, $email: user.email, $username: user.username, $password: user.password}})
+
+            return [user, null]
+        }catch (e){
+            console.log(e)
+            return [user, null]
+        }
+
+    }
 }
--- src/api/useCases/User.ts
+++ src/api/useCases/User.ts
@@ -1,6 +1,8 @@
 import BaseInputModele from "../../components/atoms/input/BaseInputModel";
 import {User as UserEntity, ValidationStates} from "../entities/User";
+import {User as UserService} from "../services/User";
 import {Result} from "../utils/useCaseResult/Result";
+import {Error} from "../utils/useCaseResult/types/Error";
 
 export class User {
 
@@ -14,9 +16,30 @@ export class User {
 
         result.errors = user.validateProperties(ValidationStates.register)
 
-        if(result.isFailed() === false){
-            result.addSuccess("user/register-success", 201)
+        if(result.isFailed()){
+            return result
         }
+
+        const userService: UserService = new UserService()
+        const [userAfterUpdatedByFirebase, errorFromFirebase] = userService.addUserToFirebase(user)
+
+        console.log(errorFromFirebase)
+
+        if(errorFromFirebase !== null){
+            result.errors.push(errorFromFirebase)
+            return result
+        }
+
+        result.content = userAfterUpdatedByFirebase
+
+        const [_, errorFromHasura]: Error = userService.addUserToHasura(user)
+
+        if(errorFromHasura !== null){
+            result.errors.push(errorFromFirebase)
+            return result
+        }
+
+        result.addSuccess("user/register-success", 201)
 
         return result
     }
